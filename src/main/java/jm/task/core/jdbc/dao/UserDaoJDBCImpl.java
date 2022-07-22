02cc2c41f7c71a5b57d6db799168d92b69b7f484
Tatiana Bycharina <tanyanavsegda1@gmail.com>
2022-07-22 16:09:28 +0300

    task 1.1.3 fixed
    task 1.1.3 fixed

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,65 +3,75 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-
-    private Util util = new Util();
-    private int count = 1;
+    private final Connection con = Util.getConnection();
 
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        String query = "CREATE TABLE users (userId INT, name VARCHAR(255), lastName VARCHAR(255), age INT)";
-        try {
-            Statement statement = util.getConnection().createStatement();
+        String query = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), lastName VARCHAR(255), age INT)";
+        try (Statement statement = con.createStatement()) {
             statement.execute(query);
+            con.commit();
         } catch (SQLException e) {
+            try {
+                con.rollback();
+            } catch (SQLException ex) {
 
+            }
         }
+
     }
 
     public void dropUsersTable() {
         String query = "DROP TABLE users";
-        try {
-            Statement statement = util.getConnection().createStatement();
+        try (Statement statement = con.createStatement()) {
             statement.execute(query);
+            con.commit();
         } catch (SQLException e) {
-            e.getMessage();
+            try {
+                con.rollback();
+            } catch (SQLException ex) {
+
+            }
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String query = "INSERT INTO users VALUES (?, ?, ?, ?)";
-        try {
-            PreparedStatement ps = util.getConnection().prepareStatement(query);
-            ps.setInt(1, count);
-            count++;
-            ps.setString(2, name);
-            ps.setString(3, lastName);
-            ps.setInt(4, age);
+        String query = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
+        try (PreparedStatement ps = con.prepareStatement(query)) {
+            ps.setString(1, name);
+            ps.setString(2, lastName);
+            ps.setInt(3, age);
             ps.executeUpdate();
+            con.commit();
         } catch (SQLException e) {
-            e.getMessage();
+            try {
+                con.rollback();
+            } catch (SQLException ex) {
+
+            }
         }
     }
 
     public void removeUserById(long id) {
-        String query = "DELETE FROM users WHERE userId=?";
-        try {
-            PreparedStatement ps = util.getConnection().prepareStatement(query);
+        String query = "DELETE FROM users WHERE id=?";
+        try (PreparedStatement ps = con.prepareStatement(query)) {
             ps.setInt(1, (int) id);
             ps.executeUpdate();
+            con.commit();
         } catch (SQLException e) {
-            e.getMessage();
+            try {
+                con.rollback();
+            } catch (SQLException ex) {
+
+            }
         }
     }
 
@@ -69,27 +79,35 @@ public class UserDaoJDBCImpl implements UserDao {
         List<User> users = new ArrayList<>();
         User user;
         String query = "SELECT * FROM users";
-        try {
-            Statement statement = util.getConnection().createStatement();
+        try (Statement statement = con.createStatement()) {
             ResultSet rs = statement.executeQuery(query);
             while (rs.next()) {
                 user = new User(rs.getString("name"), rs.getString("lastName"), rs.getByte("age"));
-                user.setId(rs.getLong("userId"));
+                user.setId(rs.getLong("id"));
                 users.add(user);
             }
+            con.commit();
         } catch (SQLException e) {
-            e.getMessage();
+            try {
+                con.rollback();
+            } catch (SQLException ex) {
+
+            }
         }
         return users;
     }
 
     public void cleanUsersTable() {
         String query = "DELETE FROM users";
-        try {
-            PreparedStatement ps = util.getConnection().prepareStatement(query);
+        try (PreparedStatement ps = con.prepareStatement(query)) {
             ps.executeUpdate();
+            con.commit();
         } catch (SQLException e) {
-            e.getMessage();
+            try {
+                con.rollback();
+            } catch (SQLException ex) {
+
+            }
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,18 +2,24 @@ package jm.task.core.jdbc.util;
 
 import java.sql.*;
 
+
 public class Util {
     // реализуйте настройку соеденения с БД
-    private Connection connection = null;
+
     public Util() {
+
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/new_schema","root", "1234");
+            connection.setAutoCommit(false);
         } catch (SQLException | ClassNotFoundException e) {
             e.getMessage();
         }
-    }
-    public Connection getConnection() {
         return connection;
     }
+
 }
